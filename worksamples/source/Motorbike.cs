975d83b4023c4ed3f3deb0228f0b214c6e56e0ac
Johanna <32160638+johannaastrom@users.noreply.github.com>
2026-02-19 14:16:55 +0100

    Bytt hårdkodad sträng till nameof

--- worksamples/source/Motorbike.cs
+++ worksamples/source/Motorbike.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TollFeeCalculator
 {
     public class Motorbike : Vehicle
     {
-        public string GetVehicleType()
-        {
-            return "Motorbike";
-        }
+        public string GetVehicleType() => nameof(Motorbike);
     }
 }
